9ac48e0268e04fe9f698318e7315039bf9b99575
Gabriel Pettier <gabriel.pettier@gmail.com>
2020-12-25 15:20:52 +0100

    fix label content

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ from kivy.lang import Builder
 
 KV = '''
 Label:
-    text: 'pretty! \nAnimation by yosif (https://lottiefiles.com/yoz) available at https://lottiefiles.com/42369-weather-wind'
-    text_size: self.width
+    text: 'pretty! \\nAnimation by yosif (https://lottiefiles.com/yoz)\\nAvailable at https://lottiefiles.com/42369-weather-wind'
+    text_size: self.width, None
     halign: 'center'
 '''
 
